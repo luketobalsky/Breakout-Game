f09314138023bb68b4e26a944c7ca50e1b6f7fb4
Baba <sheriflash@gmail.com>
2026-03-29 13:31:27 -0400

    Added Ball implementation but did not yet extend Components. Also added a few necessary ball settings to the Settings enum class.

--- ball.py
+++ ball.py
@@ -1,6 +1,69 @@
-#This will be the ball class with the associated mechanics 
-
 import pygame
-from components import Components
+from pygame import *
+from paddle import Paddle
+from brick import Brick
+from settings import Settings
 
+
+"""
+The ball class represent the blueprint necessary to create a ball object.
+"""
 class Ball:
+    collision_threshold = 5
+
+    def __init__(self, x, y):
+        self.radius = 10
+        self.width = self.radius * 2
+        self.height = self.radius * 2
+        self.x = x - self.radius
+        self.y = y
+        self.frame = Rect(self.x, self.y, self.width, self.height)
+        self.speed_x = 4
+        self.speed_y = -4
+
+    def move(self):
+        # Screen Collision Handling
+        if self.frame.left < 0 or self.frame.right > Settings.SCREEN_X:
+            self.bounce(Settings.HORIZONTAL)
+        if self.frame.top < 0:
+            self.bounce(Settings.VERTICAL)
+        if self.frame.bottom > Settings.SCREEN_Y:
+            self.game_over = -1
+
+        # Ball Movement
+        self.frame.x += self.speed_x
+        self.frame.y += self.speed_y
+
+    def bounce(self, direction: Settings):
+        match direction:
+            case Settings.VERTICAL:
+                self.speed_y *= -1
+            case Settings.HORIZONTAL:
+                self.speed_x *= -1
+
+    def hit(self, item):
+        if isinstance(item, Paddle):
+            if self.frame.colliderect(item.frame):
+                if abs(self.frame.bottom - item.frame.top) < self.collision_threshold \
+                        and self.speed_y > 0:
+                    return True
+        elif isinstance(item, Brick):
+            if self.frame.colliderect(item.frame):
+                # bounce verti
+                if abs(self.frame.bottom - item.frame.top < self.collision_threshold
+                       and self.speed_y > 0):
+                    return True
+                if abs(self.frame.top - item.frame.bottom < self.collision_threshold
+                       and self.speed_y < 0):
+                    return True
+                # bounce hori
+                if abs(self.frame.right - item.frame.left < self.collision_threshold
+                       and self.speed_x > 0):
+                    return True
+                if abs(self.frame.left - item.frame.right < self.collision_threshold
+                       and self.speed_x < 0):
+                    return True
+
+    def draw(self, screen, colour):
+        pygame.draw.circle(screen, colour,
+                           (self.frame.x + self.radius, self.frame.y + self.radius), self.radius)
--- settings.py
+++ settings.py
@@ -1,3 +1,13 @@
-#This will be the settings file to store all universal settings for the game
+# This will be the settings file to store all universal settings for the game
 
-#Add settings into here and then import any used settings in other files with the import line
+# Add settings into here and then import any used settings in other files with the import line
+from enum import Enum
+
+
+class Settings(Enum):
+    # The following are used to determine the ball bouncing Axis
+    HORIZONTAL = 1
+    VERTICAL = 2
+
+    SCREEN_X = 600
+    SCREEN_Y = 600
