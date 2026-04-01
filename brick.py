02f9110ab56437138096732506fcdda29e1fc202
luisumgc <lorellana4@student.umgc.edu>
2026-04-01 17:26:43 -0400

    Update brick.py

--- brick.py
+++ brick.py
@@ -1,7 +1,40 @@
 #This will be the bricks class with associated mechanics
+# Contributor: Luis Orellana
 
 import pygame
 from components import Components
 
-class Brick:
-    pass
+
+class Brick(Components):
+    # Class variables
+    WIDTH = 42
+    HEIGHT = 14
+
+    COLORS = {
+        "red": (255, 0, 0),
+        "green": (0, 255, 0),
+        "orange": (255, 165, 0),
+        "blue": (0, 0, 255),
+
+    }
+
+    # Constructor
+    def __init__(self, x, y, color="red"):
+        super().__init__(x, y, Brick.WIDTH, Brick.HEIGHT)
+        self.color = self.COLORS[color]
+        self.active = True
+        self.rect = pygame.Rect(self.x, self.y, self.WIDTH, self.HEIGHT)
+
+    # Methods
+
+    def draw(self, screen):
+        # Only draws if brick is active
+        if self.active:
+            pygame.draw.rect(screen, self.color,
+                             self.rect, border_radius=2)
+
+    def update(self):
+        pass  # No logic as of now
+
+    def destroy(self):
+        self.active = False
