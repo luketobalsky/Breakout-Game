2443d47eff19b148a6dd71f6a4c971e7d640d2ad
luisumgc <lorellana4@student.umgc.edu>
2026-04-04 12:44:41 -0400

    Add imports for game components in main2.py

--- main2.py
+++ main2.py
@@ -1,5 +1,19 @@
 
+import pygame
+from paddle import Paddle
+from ball import Ball
+#from brick import Brick
+from brickgrid import BrickGrid
+from scoremanager import ScoreManager
+from livesmanager import LivesManager
+#from collisionmanager import CollisionManager
+#from difficultymanager import DifficultyManager
+from renderer import Renderer
+from settings import Settings
 
+#import settings
+x = Settings.SCREEN_X.value
+y = Settings.SCREEN_Y.value
 
 
 class Game:
