e4364b8104876ce45a6b21626d5fde2478adc110
luisumgc <lorellana4@student.umgc.edu>
2026-04-12 15:10:53 -0400

    Refactor Renderer class and improve score display
    Updated the Renderer class to include settings import and adjusted score display position.

--- game/renderer.py
+++ game/renderer.py
@@ -8,34 +8,39 @@ This will set up the GUI for the game
 
 """
 import pygame
+from game.settings import Settings
+
 
 class Renderer:
-    
-    #constructor to initialize playing window
+
+    # constructor to initialize playing window
     def __init__(self, screen):
         self.screen = screen
 
-    #render components (via componenents draw() )
-    def draw(self, ball, paddle, brick_grid, livesmanager, scoremanager):         #make sure to add components here as needed
+    # render components (via componenents draw() )
+    # make sure to add components here as needed
+    def draw(self, ball, paddle, brick_grid, livesmanager, scoremanager):
 
-        #text renderer/drawer for pygame
+        # text renderer/drawer for pygame
         self.font = pygame.font.SysFont(None, 25)
 
-        #makes window specificed color
+        # makes window specificed color
         self.screen.fill("black")
 
-        #draw componenets to screen (again add componenets here as they are added done by yall)
+        # draw componenets to screen (again add componenets here as they are added done by yall)
         ball.draw(self.screen, (255, 255, 255))
         paddle.draw(self.screen)
         brick_grid.draw(self.screen)
 
-        #fetch and display current lives and scores
-        lives = self.font.render("Lives: " + str(livesmanager.current_lives()), True, "white")
-        score = self.font.render("Score: " + str(scoremanager.current_score()), True, "white")
+        # fetch and display current lives and scores
+        lives = self.font.render(
+            "Lives: " + str(livesmanager.current_lives()), True, "white")
+        score = self.font.render(
+            "Score: " + str(scoremanager.current_score()), True, "white")
 
-        #place componenets 
+        # place componenets
         self.screen.blit(lives, (10, 10))
-        self.screen.blit(score, (900, 10))
+        self.screen.blit(score, (Settings.SCREEN_X.value - 100, 10))
 
-        #update screen
+        # update screen
         pygame.display.flip()
