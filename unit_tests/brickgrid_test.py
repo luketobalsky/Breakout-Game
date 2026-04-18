264ea53bf516eb3b5772f230706ae6f33004cd89
luisumgc <lorellana4@student.umgc.edu>
2026-04-18 13:23:08 -0400

    Refactor BrickGrid tests to use Settings

--- unit_tests/brickgrid_test.py
+++ unit_tests/brickgrid_test.py
@@ -1,25 +1,25 @@
 # ----- Unit Test Brick Class -----
 from game.brickgrid import BrickGrid
-from game.brick import Brick
+from game.settings import Settings
 
 
 def test_initialization():
-    screen_width = 400
-    brick_grid = BrickGrid(screen_width)
+    brick_grid = BrickGrid()
 
     # Expected rows and columns
-    expected_columns = int(screen_width / (BrickGrid.PADDING + Brick.WIDTH))
-    expected_total_bricks = int(expected_columns * BrickGrid.ROWS)
+    expected_columns = int(Settings.SCREEN_WIDTH.value /
+                           (Settings.GRID_PADDING.value + Settings.BRICK_WIDTH.value))
+    expected_total_bricks = int(expected_columns * Settings.GRID_ROWS.value)
     assert len(brick_grid.bricks_array) == expected_total_bricks
 
 
 def test_all_bricks_destroyed_false():
-    brick_grid = BrickGrid(400)
+    brick_grid = BrickGrid()
     assert brick_grid.all_bricks_destroyed() is False
 
 
 def test_all_bricks_destroyed_true():
-    brick_grid = BrickGrid(400)
+    brick_grid = BrickGrid()
 
     # Destroy bricks
     for brick in brick_grid.bricks_array:
